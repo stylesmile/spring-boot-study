c8642a0949a42e18dbd7a0d59746a5175247a168
chenye <15002770045@139.com>
2020-05-07 17:08:08 +0800

    login

--- boot-security/src/main/java/com/example/demo/SecurityConfig.java
+++ boot-security/src/main/java/com/example/demo/SecurityConfig.java
@@ -13,10 +13,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
          * .anyRequest() :全部拦截：配置权限
          */
         http.authorizeRequests().anyRequest().hasRole("USER")//权限ROLE_USER
-                .and().formLogin().loginPage("/loginPage")//登录页面
+                .and().formLogin().loginPage("/login")//登录页面
                 .loginProcessingUrl("/login")//登录请求（自带）
                 .defaultSuccessUrl("/")//成功之后
-                .failureUrl("/loginPage")//失败之后
+                .failureUrl("/login")//失败之后
                 .permitAll()
                 .and().exceptionHandling().accessDeniedPage("/accessdenied")//没有权限的页面
                 .and().logout().logoutUrl("/logout")//登出(自带)
