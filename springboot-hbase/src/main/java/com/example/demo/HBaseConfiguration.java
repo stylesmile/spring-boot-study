d1762db0df98bf0b169c063afd4d9ce24e0beb04
stylesmile <15002770045@139.com>
2020-07-31 07:11:50 +0800

    hbase

---
+++ springboot-hbase/src/main/java/com/example/demo/HBaseConfiguration.java
@@ -0,0 +1,26 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.hadoop.hbase.HbaseTemplate;
+
+@Configuration
+public class HBaseConfiguration {
+
+    @Value("${hbase.zookeeper.quorum}")
+    private String zookeeperQuorum;
+
+    @Value("${hbase.zookeeper.property.clientPort}")
+    private String clientPort;
+
+    @Value("${zookeeper.znode.parent}")
+    private String znodeParent;
+
+    @Bean
+    public HbaseTemplate hbaseTemplate() {
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+        conf.set("hbase.zookeeper.quorum", zookeeperQuorum);
+        conf.set("hbase.zookeeper.property.clientPort", clientPort);
+        conf.set("zookeeper.znode.parent", znodeParent);
+        return new HbaseTemplate(conf);
+    }
+}
---
+++ springboot-hbase/src/main/java/com/example/demo/HBaseService.java
@@ -0,0 +1,42 @@
+@Service
+@Slf4j
+public class HBaseService {
+
+
+    @Autowired
+    private HbaseTemplate hbaseTemplate;
+
+
+    public List<Result> getRowKeyAndColumn(String tableName, String startRowkey, String stopRowkey, String column, String qualifier) {
+        FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL);
+        if (StringUtils.isNotBlank(column)) {
+            log.debug("{}", column);
+            filterList.addFilter(new FamilyFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(Bytes.toBytes(column))));
+        }
+        if (StringUtils.isNotBlank(qualifier)) {
+            log.debug("{}", qualifier);
+            filterList.addFilter(new QualifierFilter(CompareFilter.CompareOp.EQUAL, new BinaryComparator(Bytes.toBytes(qualifier))));
+        }
+        Scan scan = new Scan();
+        if (filterList.getFilters().size() > 0) {
+            scan.setFilter(filterList);
+        }
+        scan.setStartRow(Bytes.toBytes(startRowkey));
+        scan.setStopRow(Bytes.toBytes(stopRowkey));
+
+        return hbaseTemplate.find(tableName, scan, (rowMapper, rowNum) -> rowMapper);
+    }
+
+    public List<Result> getListRowkeyData(String tableName, List<String> rowKeys, String familyColumn, String column) {
+        return rowKeys.stream().map(rk -> {
+            if (StringUtils.isNotBlank(familyColumn)) {
+                if (StringUtils.isNotBlank(column)) {
+                    return hbaseTemplate.get(tableName, rk, familyColumn, column, (rowMapper, rowNum) -> rowMapper);
+                } else {
+                    return hbaseTemplate.get(tableName, rk, familyColumn, (rowMapper, rowNum) -> rowMapper);
+                }
+            }
+            return hbaseTemplate.get(tableName, rk, (rowMapper, rowNum) -> rowMapper);
+        }).collect(Collectors.toList());
+    }
+}
