4c416a9ba5c7c8e262901520b192bf655fe9eba6
stylesmile <15002770045@139.com>
2020-08-09 10:24:40 +0800

    springboot-mybatis-multi-mysql-postgre sql

---
+++ gradle-projects/springboot-mybatis-multi-mysql-postgre/third_user.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 39.97.250.105_5432
+ Source Server Type    : PostgreSQL
+ Source Server Version : 120003
+ Source Host           : 39.97.250.105:5432
+ Source Catalog        : test3
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 120003
+ File Encoding         : 65001
+
+ Date: 09/08/2020 10:23:46
+*/
+
